2fc77b2cf83f8b72455cb4e3ba4049d8c6f705ad
Shivam <shivam@Shivams-MacBook-Air.local>
2026-05-16 19:55:34 +0530

    Obsidiean Push

---
+++ vault/static/katex-init.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+  if (window.renderMathInElement) {
+    renderMathInElement(document.body, {
+      delimiters: [
+        { left: "$$", right: "$$", display: true },
+        { left: "$", right: "$", display: false },
+        { left: "\\(", right: "\\)", display: false },
+        { left: "\\[", right: "\\]", display: true }
+      ],
+      throwOnError: false
+    });
+  }
+});
